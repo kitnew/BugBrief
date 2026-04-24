11bf07a973b63dde17b7bc4166c94955e967f2fe
neVlados00 <vlogpro98@gmail.com>
2026-04-24 23:48:04 +0200

    refactor(frontend): remove unused navigation behavior setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import './NavFix.css'
-import { setupNavigationBehavior } from './navigationBehavior'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-
-setupNavigationBehavior()
