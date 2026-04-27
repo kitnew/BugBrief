656e81b5299cc782ffdfcff73374cdebf6b0be5c
Nikita Chernysh <h3s0y4mchik@gmail.com>
2026-04-27 15:58:57 +0200

    fix(backend): API Routing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,16 +5,24 @@ import { initSchema } from './db.js';
 import bugRoutes from './routes/bugRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://main.d8h80raj6aco8.amplifyapp.com"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 // Підключаємо наші маршрути
-app.use('/', bugRoutes); 
+app.use('/', bugRoutes);
 
 initSchema().then(() => {
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+  app.listen(PORT as number, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
   });
 });
